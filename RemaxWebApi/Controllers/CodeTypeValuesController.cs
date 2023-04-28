aa556039d355411d8989fe3aba0d88973b8c4273
Keshava L <kesmurt0@publicisgroupe.net>
2023-04-27 23:43:17 -0400

    values

--- RemaxWebApi/Controllers/CodeTypeValuesController.cs
+++ RemaxWebApi/Controllers/CodeTypeValuesController.cs
@@ -24,6 +24,41 @@ namespace RemaxWebApi.Controllers
             var product = await _context.CodeTypeValues.Where(m => m.CodeTypeShortCode == codeType).ToListAsync();            
             return Ok(product);
         }
+        [HttpPut]       
+        public async Task<IActionResult> Edit([FromBody] CodeTypeValues codeTypes)
+        {
+            try
+            {
+                _context.CodeTypeValues.Update(codeTypes);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{shortCode}")]        
+        public async Task<int> Delete(string shortCode)
+        {
+            try
+            {
+                CodeTypeValues? codeTypes = _context.CodeTypeValues.FirstOrDefault(x => x.ShortCode == shortCode);
+                if (codeTypes != null)
+                {
+                    _context.CodeTypeValues.Remove(codeTypes);
+                    return await _context.SaveChangesAsync();
+                }
+                else
+                    return -1;
+                    
+                //return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return -1;
+            }
+        }
         [HttpPost]
         public async Task<IActionResult> InsertCodeTypeVlaues([FromBody] CodeTypeValues codeTypevalues)
         {
