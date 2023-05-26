4f9a30b5c0671e255bf08005630d76455e551038
REAPI2023 <reapi2023@gmail.com>
2023-05-26 18:08:26 +0530

    Updated phnumber

--- RemaxWebApi/Models/Leads.cs
+++ RemaxWebApi/Models/Leads.cs
@@ -7,7 +7,7 @@ namespace RemaxWebAPI.Models
         [Key]
         public int LeadId { get; set; }
         public string Name { get; set; }
-        public int PhNumber { get; set; }
+        public string PhNumber { get; set; }
         public decimal Budget { get; set; }
         public string? Criteria { get; set; }
         public string? LeadStatus { get; set; }
