822c9335433c75aaac701a3e99e63eadd6f77d2d
AswinProsper <aswnmalapaka@gmail.com>
2023-05-29 08:28:48 +0530

    Updated db changes

---
+++ RemaxWebApi/Migrations/20230529025715_Create_14.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RemaxWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_14 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ModulePermissions",
+                columns: table => new
+                {
+                    ModuleShortName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ModuleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Permission = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModulePermissions", x => x.ModuleShortName);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ModulePermissions");
+        }
+    }
+}
