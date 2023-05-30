da430e68b55f1275d02fc2db1b892488ad9c2848
AswinProsper <aswnmalapaka@gmail.com>
2023-05-30 09:35:04 +0530

    User table added. Publish plan added

---
+++ RemaxWebApi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace RemaxWebApi.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }  
+        public string PhNumber { get; set; }
+        public string Role { get; set; }
+    }
+}
