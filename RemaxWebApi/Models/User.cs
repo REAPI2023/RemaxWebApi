2f987e1b067f15d5e0f1f6e60bb29f61d0108582
AswinProsper <aswnmalapaka@gmail.com>
2023-05-30 12:55:14 +0530

    Updated publish

--- RemaxWebApi/Models/User.cs
+++ RemaxWebApi/Models/User.cs
@@ -1,7 +1,12 @@
-﻿namespace RemaxWebApi.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace RemaxWebApi.Models
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }  
         public string PhNumber { get; set; }
